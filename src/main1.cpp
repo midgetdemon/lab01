1cf3b68c0e79020694fed28a52407bfcb3343852
midgetdemon <zinalp19@gmail.com>
2021-01-13 14:17:45 -0800

    Created single module for testing with gtest
    *Added c-echo file for testing and another main file to create a test executable

--- src/main1.cpp
+++ src/main1.cpp
@@ -5,14 +5,14 @@
 using namespace std;
 
 int main(){
-Rectangle rect, rect2;
-rect.set_width(3);
-rect.set_height(4);
+Rectangle rect1, rect2;
+rect1.set_width(3);
+rect1.set_height(4);
 
 rect2.set_width(4);
 rect2.set_height(2);
 
-cout << "Rectangle 1 area: " << rect2.area() << endl;
+cout << "Rectangle 1 area: " << rect1.area() << endl;
 cout << "Rectangle 2 area: " << rect2.area() << endl;
 
 return 0;
